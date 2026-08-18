d960ad45d824b4e093d06c1b65d519145eec09e7
Manish Maharjan <manishnewar26@gmail.com>
2026-08-18 16:12:57 +0100

    prettier applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import './App.css'
+import './App.css';
 import Navbar from './layouts/Navbar';
 import HeroSection from './layouts/HeroSection';
 import FeatureSection from './layouts/FeatureSection';
 import Footer from './layouts/Footer';
 
 function App() {
-  return (
-    <div className="page-shell">
-      <Navbar/>
+    return (
+        <div className="page-shell">
+            <Navbar />
 
-      <main className="main-content">
-        <HeroSection/>
+            <main className="main-content">
+                <HeroSection />
 
-        <FeatureSection/>
-      </main>
+                <FeatureSection />
+            </main>
 
-      <Footer/>
-    </div>
-  )
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
