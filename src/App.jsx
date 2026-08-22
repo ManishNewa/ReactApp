d523fe43ed1ab83b0e6bb02eebcf20539f3e4e83
Manish Maharjan <manishnewar26@gmail.com>
2026-08-22 19:05:13 +0100

    add circle background on chevron

--- src/App.jsx
+++ src/App.jsx
@@ -63,18 +63,22 @@ function App() {
                                     key={feature.id}
                                     className="flex items-center gap-4"
                                 >
-                                    <svg
-                                        className="w-3.5 h-3.5"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
-                                    <span className="text-md font-medium text-slate-400">{feature.value}</span>
+                                    <div className="bg-white/20 flex justify-center items-center shrink-0 w-5 h-5 rounded-full">
+                                        <svg
+                                            className="w-3.5 h-3.5"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                                clipRule="evenodd"
+                                            />
+                                        </svg>
+                                    </div>
+                                    <span className="text-md font-medium text-slate-400">
+                                        {feature.value}
+                                    </span>
                                 </li>
                             ))}
                         </ul>
