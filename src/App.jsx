baf92d25802a435fc096adb11b67e2ccba755e43
Manish Maharjan <manishnewar26@gmail.com>
2026-08-22 18:14:53 +0100

    configure text color for button and paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ function App() {
                 <h1 className="text-white text-[32px] font-bold">
                     Simple, transparent pricing
                 </h1>
-                <p className="text-white mb-4">
+                <p className="text-slate-400 mb-4 font-semibold">
                     No contracts. No suprise fees.
                 </p>
                 <div className="flex p-2 gap-5 bg-[#1E293B] border-2 border-[#263145] rounded-[50px]">
                     <button className="uppercase px-5 py-2 bg-[#6366F1] rounded-[50px] font-semibold text-white">
                         Monthly
                     </button>
-                    <button className="uppercase px-5 py-2 font-semibold text-white">
+                    <button className="uppercase px-5 py-2 font-semibold text-slate-400">
                         Yearly
                     </button>
                 </div>
