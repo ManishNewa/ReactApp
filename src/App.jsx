1328125524c68a05fb43d43ebb3655c971a423db
Manish Maharjan <manishnewar26@gmail.com>
2026-08-22 18:39:08 +0100

    add css formats to contents and setup svg on the lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
 function App() {
+    const features1 = [
+        {
+            key: 1,
+            value: 'All limited links',
+        },
+        {
+            key: 2,
+            value: 'Own analytics platform',
+        },
+        {
+            key: 3,
+            value: 'Chat support',
+        },
+        {
+            key: 4,
+            value: 'Optimize hashtags',
+        },
+        {
+            key: 5,
+            value: 'Unlimited users',
+        },
+    ];
     return (
         <main className="min-h-screen bg-[#0F172A] py-[10vh]">
             <section className="hero-section flex flex-col items-center justify-center gap-2">
@@ -19,25 +41,42 @@ function App() {
             </section>
             <section className="pricing-section w-full max-w-[260]">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-4">
-                    <div className="card bg-slate-800 text-white ">
+                    <div className="bg-slate-800 text-white p-8 border border-slate-500 flex flex-col rounded-e-3xl">
                         {/* Popular Tag */}
-                        <span>MOST POPULAR</span>
+                        {/* <span>MOST POPULAR</span> */}
                         <div className="price">
-                            <div className="price-name">
-                                <h1 className="text-white">$80</h1>/month
+                            <div className="flex items-baseline">
+                                <h1 className="text-white text-bold text-4xl">
+                                    $80
+                                </h1>
+                                <span>/month</span>
                             </div>
-                            <div className="price-category">Base</div>
+                            <div className="text-2xl font-semibold">Base</div>
                         </div>
-                        <p>
+                        <p className="text-slate-400">
                             For most businesses that want to optimize web
                             queries
                         </p>
-                        <ul className="category-features">
-                            <li>All limited links</li>
-                            <li>Own analytics platform</li>
-                            <li>Chat support</li>
-                            <li>Optimize hashtags</li>
-                            <li>Unlimited users</li>
+                        <ul className="flex-1 mb-8">
+                            {features1.map((feature) => (
+                                <li
+                                    key={feature.id}
+                                    className="flex items-center gap-2"
+                                >
+                                    <svg
+                                        className="w-3.5 h-3.5"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                    {feature.value}
+                                </li>
+                            ))}
                         </ul>
                         <button className="text-white text-center bg-[#1E293B] py-3 px-10 rounded-2xl">
                             Downgrade
