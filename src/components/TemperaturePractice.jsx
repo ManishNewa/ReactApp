48d3a31043e162e0833b908997a21ce6e60c129a
Manish Maharjan <manishnewar26@gmail.com>
2026-08-23 18:27:51 +0100

    use function from useState to re render temperature changes

--- src/components/TemperaturePractice.jsx
+++ src/components/TemperaturePractice.jsx
@@ -3,10 +3,20 @@
 // Reset to 20°C
 // Show "Cold", "Normal", or "Hot"
 
+import { useState } from 'react';
+
 const START_TEMP = 20;
 
 function TemperaturePractice() {
-    const temperature = START_TEMP;
+    const [temperature, setTemperature] = useState(START_TEMP);
+
+    function changeTemperature(value) {
+        if ([1, -1].includes(value)) {
+            setTemperature(temperature + value);
+        } else {
+            setTemperature(START_TEMP);
+        }
+    }
 
     return (
         <div className="min-h-screen flex items-center justify-center bg-linear-to-b from-amber-100 to-orange-200 p-6">
@@ -22,15 +32,24 @@ function TemperaturePractice() {
                 </div>
 
                 <div className="mt-8 flex items-center justify-center gap-3">
-                    <button className="rounded-lg bg-sky-600 px-5 py-2 text-xl font-bold text-white transition hover:bg-sky-700">
+                    <button
+                        onClick={() => changeTemperature(-1)}
+                        className="rounded-lg bg-sky-600 px-5 py-2 text-xl font-bold text-white transition hover:bg-sky-700"
+                    >
                         -
                     </button>
 
-                    <button className="rounded-lg border border-gray-300 bg-gray-100 px-4 py-2 text-sm font-semibold text-gray-700 transition hover:bg-gray-200">
+                    <button
+                        onClick={() => changeTemperature()}
+                        className="rounded-lg border border-gray-300 bg-gray-100 px-4 py-2 text-sm font-semibold text-gray-700 transition hover:bg-gray-200"
+                    >
                         Reset to 20°C
                     </button>
 
-                    <button className="rounded-lg bg-rose-600 px-5 py-2 text-xl font-bold text-white transition hover:bg-rose-700">
+                    <button
+                        onClick={() => changeTemperature(1)}
+                        className="rounded-lg bg-rose-600 px-5 py-2 text-xl font-bold text-white transition hover:bg-rose-700"
+                    >
                         +
                     </button>
                 </div>
