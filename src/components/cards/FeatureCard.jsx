d8827a0832110322b421203b0bfbf8471492ec80
Manish Maharjan <manishnewar26@gmail.com>
2026-08-23 17:35:40 +0100

    code cleanup

--- src/components/cards/FeatureCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function FeatureCard({ feature }) {
-    return (
-        <article className="feature-card">
-            <div className="feature-icon">{feature.order}</div>
-            <h3>{feature.title}</h3>
-            <p>{feature.description}</p>
-        </article>
-    );
-}
