1c8d45c25e1ea64499b60508a9f453cb088970b2
Manish Maharjan <manishnewar26@gmail.com>
2026-08-23 17:26:58 +0100

    code cleanup for initial setup boiler plate

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,5 @@
-import './App.css';
-import Navbar from './layouts/Navbar';
-import Hero from './layouts/Hero';
-import Features from './layouts/Features';
-import Footer from './layouts/Footer';
-
 function App() {
-    return (
-        <div className="page-shell">
-            <Navbar />
-
-            <main className="main-content">
-                <Hero />
-
-                <Features />
-            </main>
-
-            <Footer />
-        </div>
-    );
+    return <></>;
 }
 
 export default App;
