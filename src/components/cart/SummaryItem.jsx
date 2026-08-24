3d97181e7e095376151dc4e463902d974386ac7e
Manish Maharjan <manishnewar26@gmail.com>
2026-08-24 16:38:38 +0100

    text size increased a bit

--- src/components/cart/SummaryItem.jsx
+++ src/components/cart/SummaryItem.jsx
@@ -1,6 +1,6 @@
 export default function SummaryItem({ label, amount }) {
     return (
-        <div className="flex justify-between items-center text-sm">
+        <div className="flex justify-between items-center text-md">
             <span className="capitalize font-bold text-gray-600">{label}:</span>
             <span className="font-semibold text-gray-900">{amount?.toFixed(2)}</span>
         </div>
