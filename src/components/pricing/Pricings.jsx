afe2ff4fd0213de7fe35f9c9f065a0f6d268707b
Manish Maharjan <manishnewar26@gmail.com>
2026-08-24 14:44:49 +0100

    move className to pricings component

--- src/components/pricing/Pricings.jsx
+++ src/components/pricing/Pricings.jsx
@@ -27,26 +27,28 @@ export default function Pricings() {
     ];
 
     return (
-        <div className="flex flex-col items-center justify-center gap-2">
-            <h1 className="text-white text-[32px] font-bold mb-4">
-                Simple, transparent pricing
-            </h1>
-            <p className="text-lg text-slate-400 mb-8 font-semibold">
-                No contracts. No suprise fees.
-            </p>
-            <div className="flex p-2 mb-10 gap-5 bg-[#1E293B] border-2 border-[#263145] rounded-[50px]">
-                <button className="uppercase px-5 py-3 bg-indigo-400 rounded-[50px] font-semibold text-white hover:cursor-pointer">
-                    Monthly
-                </button>
-                <button className="uppercase px-5 py-3 font-semibold text-slate-400 hover:cursor-pointer">
-                    Yearly
-                </button>
-            </div>
-            <div className="pricing-section w-full max-w-[260]">
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-10">
-                    {types.map((type) => (
-                        <PricingCard features={features} {...type} />
-                    ))}
+        <div className="min-h-screen bg-[#0F172A] flex flex-col items-center py-20 px-5 md:px-10 mx-auto">
+            <div className="flex flex-col items-center justify-center gap-2">
+                <h1 className="text-white text-[32px] font-bold mb-4">
+                    Simple, transparent pricing
+                </h1>
+                <p className="text-lg text-slate-400 mb-8 font-semibold">
+                    No contracts. No suprise fees.
+                </p>
+                <div className="flex p-2 mb-10 gap-5 bg-[#1E293B] border-2 border-[#263145] rounded-[50px]">
+                    <button className="uppercase px-5 py-3 bg-indigo-400 rounded-[50px] font-semibold text-white hover:cursor-pointer">
+                        Monthly
+                    </button>
+                    <button className="uppercase px-5 py-3 font-semibold text-slate-400 hover:cursor-pointer">
+                        Yearly
+                    </button>
+                </div>
+                <div className="pricing-section w-full max-w-[260]">
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-10">
+                        {types.map((type) => (
+                            <PricingCard features={features} {...type} />
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
