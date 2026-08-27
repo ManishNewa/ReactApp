d2e310d8af8b00d1ac2e1985af535ea479ae7c9c
Manish Maharjan <manishnewar26@gmail.com>
2026-08-27 07:30:04 +0100

    remove bold css for order summary labels, add USD on amount

--- src/components/cart/SummaryItem.jsx
+++ src/components/cart/SummaryItem.jsx
@@ -1,8 +1,10 @@
 export default function SummaryItem({ label, amount }) {
     return (
-        <div className="flex justify-between items-center text-md">
-            <span className="capitalize font-bold text-gray-600">{label}:</span>
-            <span className="font-semibold text-gray-900">{amount?.toFixed(2)}</span>
+        <div className="flex justify-between items-center text-md mb-1">
+            <span className="capitalize text-gray-600">{label}:</span>
+            <span className=" font-semibold text-gray-900">
+                {amount?.toFixed(2)} USD
+            </span>
         </div>
     );
 }
