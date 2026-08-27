6ffb2ecb7fb61afe6377f03996031ed22a8f43eb
Manish Maharjan <manishnewar26@gmail.com>
2026-08-27 07:46:29 +0100

    move cart section contents to sub component

---
+++ src/components/cart/CartSection.jsx
@@ -0,0 +1,48 @@
+import CartItem from './CartItem';
+
+export default function CartSection({ items, setCartItems, className }) {
+    function handleQuantityChange(id, amount) {
+        const updatedItems = items.map((item) => {
+            if (item.id === id && item.quantity >= 0) {
+                item.quantity += amount;
+            }
+            return item;
+        });
+        setCartItems(updatedItems);
+    }
+
+    function handleItemDeletion(id) {
+        const updatedItems = items.filter((item) => item.id !== id);
+        setCartItems(updatedItems);
+    }
+
+    return (
+        <div className={className}>
+            <table className="w-full">
+                <thead className="border-b border-gray-100">
+                    <tr className="text-sm font-semibold">
+                        <th className="text-left pb-6">Product</th>
+                        <th className="w-40 text-left pb-6">Quantity</th>
+                        <th className="w-20 text-left pb-6">Total</th>
+                        <th className="w-10 text-left pb-6">Action</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {items.length ? (
+                        items.map((item) => (
+                            <CartItem
+                                key={item.id}
+                                item={item}
+                                onQuantityChange={handleQuantityChange}
+                                onItemDelete={handleItemDeletion}
+                            />
+                        ))
+                    ) : (
+                        <p>Your cart list is empty</p>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    );
+}
--- src/components/cart/ShoppingCart.jsx
+++ src/components/cart/ShoppingCart.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { cartItems } from '../../data/ShoppingCartData';
 
-import CartItem from './CartItem';
+import CartSection from './CartSection';
 import SummarySection from './SummarySection';
 
 export default function ShoppingCart() {
@@ -11,21 +11,6 @@ export default function ShoppingCart() {
         return total + item.price * item.quantity;
     }, 0);
 
-    function handleQuantityChange(id, amount) {
-        const updatedItems = items.map((item) => {
-            if (item.id === id && item.quantity >= 0) {
-                item.quantity += amount;
-            }
-            return item;
-        });
-        setCartItems(updatedItems);
-    }
-
-    function handleItemDeletion(id) {
-        const updatedItems = items.filter((item) => item.id !== id);
-        setCartItems(updatedItems);
-    }
-
     return (
         <main className="min-h-screen">
             <div className="max-w-6xl mx-auto mt-15">
@@ -33,42 +18,15 @@ export default function ShoppingCart() {
                     Shopping Cart
                 </h1>
                 <div className="flex gap-8">
-                    <div className="w-full border border-gray-300 rounded-xl p-4">
-                        <table className="w-full">
-                            <thead className="border-b border-gray-100">
-                                <tr className="text-sm font-semibold">
-                                    <th className="text-left pb-6">Product</th>
-                                    <th className="w-40 text-left pb-6">
-                                        Quantity
-                                    </th>
-                                    <th className="w-20 text-left pb-6">
-                                        Total
-                                    </th>
-                                    <th className="w-10 text-left pb-6">
-                                        Action
-                                    </th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                {items.length ? (
-                                    items.map((item) => (
-                                        <CartItem
-                                            key={item.id}
-                                            item={item}
-                                            onQuantityChange={
-                                                handleQuantityChange
-                                            }
-                                            onItemDelete={handleItemDeletion}
-                                        />
-                                    ))
-                                ) : (
-                                    <p>Your cart list is empty</p>
-                                )}
-                            </tbody>
-                        </table>
-                    </div>
-                    <SummarySection className="w-95" subTotalAmount={subTotalAmount} />
+                    <CartSection
+                        className="w-full border border-gray-300 rounded-xl p-4"
+                        items={items}
+                        setCartItems={setCartItems}
+                    />
+                    <SummarySection
+                        className="w-95"
+                        subTotalAmount={subTotalAmount}
+                    />
                 </div>
             </div>
         </main>
