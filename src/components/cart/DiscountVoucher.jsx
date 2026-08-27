2ca44ba139ecac33b7a9522383dadeeb4658db3e
Manish Maharjan <manishnewar26@gmail.com>
2026-08-27 08:49:44 +0100

    add voucher value based operation, css adjusted to check if voucher is input before making valid button click events

--- src/components/cart/DiscountVoucher.jsx
+++ src/components/cart/DiscountVoucher.jsx
@@ -1,12 +1,24 @@
-export default function DiscountVoucher() {
+import { useState } from 'react';
+
+export default function DiscountVoucher({ voucherApplied }) {
+    const [voucher, setVoucher] = useState('');
+
+    const hasVoucherValue = voucher.trim() !== '';
+
     return (
         <div className="flex gap-3">
             <input
                 type="text"
+                value={voucher}
+                onChange={(e) => setVoucher(e.target.value)}
                 placeholder="Discount Voucher"
                 className="border rounded-full focus:outline-gray-500 px-3 border-gray-200"
             />
-            <button className="border border-gray-200 px-6 py-2 rounded-full font-semibold text-sm hover:cursor-pointer hover:scale-105">
+            <button
+                disabled={!hasVoucherValue}
+                className={`border px-6 py-2 rounded-full font-semibold text-sm ${hasVoucherValue ? 'border-gray-400 hover:cursor-pointer hover:scale-105 ' : 'border-gray-200'}`}
+                onClick={() => voucherApplied(voucher)}
+            >
                 Apply
             </button>
         </div>
