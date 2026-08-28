64f1c676c0efd78d8e7a5e0fd9c26d20b3e01960
Manish Maharjan <manishnewar26@gmail.com>
2026-08-28 15:09:52 +0100

    add products

---
+++ src/datasets/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+        id: 1,
+        name: 'Canvas Backpack',
+        category: 'Bags',
+        price: 48.0,
+    },
+    {
+        id: 2,
+        name: 'Ceramic Coffee Mug',
+        category: 'Kitchen',
+        price: 16.5,
+    },
+    {
+        id: 3,
+        name: 'Everyday Notebook',
+        category: 'Stationery',
+        price: 12.0,
+    },
+    {
+        id: 4,
+        name: 'Linen Table Lamp',
+        category: 'Home',
+        price: 72.0,
+    },
+    {
+        id: 5,
+        name: 'Wool Knit Scarf',
+        category: 'Accessories',
+        price: 34.0,
+    },
+];
