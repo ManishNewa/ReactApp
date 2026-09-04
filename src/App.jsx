9c9fda848e0acf9ddb1e9bd5b6cc07e446c5f757
Manish Maharjan <manishnewar26@gmail.com>
2026-09-04 11:12:38 +0100

    setup dynamic routes to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { Contact } from './pages/Contact';
+import { About } from './pages/About';
+import { NotFound } from './pages/NotFound';
+
+const routes = [
+    {
+        path: '/',
+        element: <Home />,
+    },
+    {
+        path: '/contact',
+        element: <Contact />,
+    },
+    {
+        path: '/about',
+        element: <About />,
+    },
+    {
+        path: '/about',
+        element: <About />,
+    },
+];
+
 function App() {
-    return <></>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                {routes.map((route) => (
+                    <Route
+                        key={route.path}
+                        path={route.path}
+                        element={route.element}
+                    />
+                ))}
+
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
