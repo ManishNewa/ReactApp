e1f352d156e49f3572b68bcc67c9dfec76243a9b
Manish Maharjan <manishnewar26@gmail.com>
2026-09-04 15:38:00 +0100

    initialize pages for exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { DashboardLayout } from './pages/dashboard/DashboardLayout';
+import { Users } from './pages/dashboard/Users';
+import { UserDetails } from './pages/dashboard/UserDetails';
+import { NotFound } from './pages/NotFound';
+
 function App() {
-    return <></>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/dashboard" element={<DashboardLayout />}>
+                    <Route
+                        index
+                        element={<h1>This is dashdbaord overview section</h1>}
+                    />
+                    <Route path="users" element={<Users />} />
+                    <Route path="users/:userId" element={<UserDetails />} />
+                </Route>
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
