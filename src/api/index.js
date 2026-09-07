310a4fac77d0e4bfcb8ad605165588e2f41794d3
Manish Maharjan <manishnewar26@gmail.com>
2026-09-07 15:14:44 +0100

    add api key to movie api url

--- src/api/index.js
+++ src/api/index.js
@@ -2,4 +2,7 @@ import axios from 'axios';
 
 export const MovieAPI = axios.create({
     baseURL: import.meta.env.MOVIE_BASE_URL,
+    params: {
+        api_key: import.meta.env.API_ACCESS_TOKEN,
+    },
 });
