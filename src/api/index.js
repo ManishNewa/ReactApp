ab3d5fd5b9e32b2be090ee781701c936a298d768
Manish Maharjan <manishnewar26@gmail.com>
2026-09-07 21:05:26 +0100

    fix api url and access key

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const MovieAPI = axios.create({
-    baseURL: import.meta.env.MOVIE_BASE_URL,
+    baseURL: import.meta.env.VITE_MOVIE_BASE_URL,
     params: {
-        api_key: import.meta.env.API_ACCESS_TOKEN,
+        api_key: import.meta.env.VITE_API_ACCESS_KEY,
     },
 });
