8da98030f09fb5facc66e134338e272d18650e01
Manish Maharjan <manishnewar26@gmail.com>
2026-09-09 15:43:53 +0100

    add skeleton loader on trending movies below hero section

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,5 +1,5 @@
-import { MovieCard } from './cards/MovieCard';
-export function Trending({ movies }) {
+import { MovieCard, SkeletonMovieCard } from './cards/MovieCard';
+export function Trending({ movies, isLoading }) {
     return (
         <div className="py-6 px-12 relative">
             <h2 className="text-white text-xl font-bold mb-4 tracking-wider uppercase">
@@ -24,9 +24,13 @@ export function Trending({ movies }) {
                 </button>
 
                 <div className="flex space-x-4 border border-white h-20 overflow-x-auto scrollbar-hide snap-x scroll-smooth pb-4">
-                    {movies.map((movie) => (
-                        <MovieCard key={movie.id} movie={movie} />
-                    ))}
+                    {isLoading
+                        ? Array.from({ length: 10 }, (_, index) => (
+                              <SkeletonMovieCard key={index} />
+                          ))
+                        : movies.map((movie) => (
+                              <MovieCard key={movie.id} movie={movie} />
+                          ))}
                 </div>
 
                 <button className="absolute right-0 top-1/2 -translate-y-1/2 z-10 bg-black/50 hover:bg-black/80 text-white p-2 rounded-full opacity-0 group-hover:opacity-100 transition-opacity hidden md:block -mr-4">
