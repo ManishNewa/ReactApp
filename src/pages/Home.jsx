312257eab565dea0d4b5faf503ab614d2ac5dff8
Manish Maharjan <manishnewar26@gmail.com>
2026-09-09 14:46:32 +0100

    set trending movies api call

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from 'react';
 import { Hero } from '../components/Hero';
 import { Trending } from '../components/Trending';
+import { movieAPI } from '../api';
 
 export function Home() {
+    const [trendingMovies, setTrendingMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchTrendingMovies() {
+            const response = await movieAPI.get('/trending/movie/week');
+            setTrendingMovies(response.data.results);
+        }
+
+        fetchTrendingMovies();
+    }, []);
+
     return (
         <div className="h-full overflow-hidden">
             <Hero />
