780c9db75e1e2411192d756f99108995cda5f462
Manish Maharjan <manishnewar26@gmail.com>
2026-09-09 14:35:28 +0100

    setup axios instance

---
+++ src/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export const movieAPI = axios.create({
+    baseURL: import.meta.env.VITE_MOVIE_BASE_URL,
+    params: {
+        api_key: import.meta.env.VITE_API_ACCESS_KEY,
+    },
+});
